2bc88d291a1016634aec6691037625c881bd1fc6
churchbob <churchtalker@gmail.com>
2025-04-10 15:20:59 -0500

    Add files via upload

---
+++ DragAndDrop/controls.js
@@ -0,0 +1,83 @@
+var contrls = 
+{
+  "Quiz": [
+    {
+		"id": 1,
+		"name": "Item A",
+		"character": "Charmandar",
+		"avatar":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/avatars/Slide1.PNG",
+		"screen":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/screens/Slide1.png",
+		"answer": [1,2]
+    },
+    {
+		"id": 2,
+		"name": "Item B",
+		"character": "Bulsaur",
+		"avatar":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/avatars/Slide2.PNG",
+		"screen":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/screens/Slide2.png",
+		"answer": [2,3]
+    },
+    {
+		"id": 3,
+		"name": "Item C",
+		"character": "Pikachu",
+		"avatar":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/avatars/Slide3.PNG",
+		"screen": "https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/screens/Slide3.png",
+		"answer": [3,4]
+    },
+	 {
+		"id": 4,
+		"name": "Item D",
+		"character": "Squirtle",
+		"avatar":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/avatars/Slide4.PNG",
+		"screen":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/screens/Slide4.png",
+		"answer": [4,5]
+    }
+  ],
+  "Cards":	[
+	{
+		"id":1,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide1.png",
+		"desc":"Medicare"
+	},{
+		"id":2,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide2.png",
+		"desc":"employer"
+	},{
+		"id":3,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide4.png",
+		"desc":"Black Lung"
+	},{
+		"id":4,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide4.png",
+		"desc":"Cobra"
+
+	},{
+		"id":5,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide5.png",
+		"desc":"Tricare"
+
+	},{
+		"id":6,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide6.png",
+		"desc":"Workman's Compensation"
+
+	},{
+		"id":7,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide7.png",
+		"desc":"Medicaid"
+
+	},{
+		"id":8,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide8.png",
+		"desc":"Medigap"
+
+	},{
+		"id":9,
+		"src":"https://bjc.sharepoint.com/:i:/r/sites/BobsProjects/TestSite/drag/Shared%20Documents/ins2/Slide9.png",
+		"desc":"Veteran's Health"
+
+	}
+	
+  ]
+}
